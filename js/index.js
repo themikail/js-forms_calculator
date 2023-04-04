c41d4e88b6a9fb587056697307ec904ab514f1dc
themikail <hello@mikailgokce.com>
2023-04-04 22:57:43 +0200

    add code

--- js/index.js
+++ js/index.js
@@ -22,9 +22,31 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const numberA = parseFloat(document.querySelector("#number-a").value);
+  const numberB = parseFloat(document.querySelector("#number-b").value);
+  const operator = document.querySelector('[name="operator"]:checked').value;
+
   let result;
 
   // --v-- write your code here --v--
+
+  switch (operator) {
+    case "addition":
+      result = add(numberA, numberB);
+      break;
+    case "subtraction":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiplication":
+      result = multiply(numberA, numberB);
+      break;
+    case "division":
+      result = divide(numberA, numberB);
+      break;
+    default:
+      result = "Invalid operator";
+      break;
+  }
 
   // --^-- write your code here --^--
 
